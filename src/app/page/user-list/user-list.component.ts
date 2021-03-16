e5bbc07ff99e0ffacd5de6dd5523622c0a73f9bb
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-03-16 19:31:24 +0100

    delete, sorter, filter

--- src/app/page/user-list/user-list.component.ts
+++ src/app/page/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/service/user.service';
@@ -12,11 +13,40 @@ export class UserListComponent implements OnInit {
 
   users$: Observable<User[]> = this.userService.getAll();
 
+  filterPhrase: string = '';
+  filterKey: string = 'name';
+
+  sortby: string = 'id';
+  sorterDirection: number = 0;
+
   constructor(
     private userService: UserService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.userService.getAll()
   }
 
-}
+  onDelete(user: User): void {
+    this.userService.remove(user).subscribe(
+      () => {
+        this.userService.getAll();
+      }
+    );
+  }
+
+  changeOrder(param: string): void {
+    if (this.sortby === '' || this.sortby != param) {
+      this.sorterDirection = 1;
+    }
+    if (this.sortby === param) {
+      if (this.sorterDirection === 1) this.sorterDirection = 2;
+      else this.sorterDirection = 1;
+    }
+    this.sortby = param;
+  }
+
+
+  }
+
